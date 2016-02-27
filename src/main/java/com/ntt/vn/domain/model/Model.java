85a84b0f08f5cd79b59f86efb5a0f3d0e82677d0
Nguyen Luong Bang <luongbangvh@gmail.com>
2016-02-27 14:49:59 +0700

    add git ignore

---
+++ src/main/java/com/ntt/vn/domain/model/Model.java
@@ -0,0 +1,16 @@
+package com.ntt.vn.domain.model;
+
+/**
+ * Created by bangnl on 2/27/16.
+ */
+public class Model {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
