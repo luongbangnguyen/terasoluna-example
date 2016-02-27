28f2b0bab51efa35d1bac185be6c8ed9bd844062
Nguyen Luong Bang <luongbangvh@gmail.com>
2016-02-27 15:54:33 +0700

    add test class for user service

---
+++ src/test/java/com/ntt/vn/service/TestUserService.java
@@ -0,0 +1,29 @@
+package com.ntt.vn.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by bangnl on 2/27/16.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class TestUserService {
+    @Test
+    public void testSaveUser(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testUpdateUser(){
+        assertTrue(false);
+    }
+
+    @Test
+    public void testDeleteUser(){
+        assertTrue(false);
+    }
+
+}
